d85a0ac3aa6049e072f9b971de9ef3b8569ab811
bruno <bruno@pop-os.localdomain>
2023-09-15 18:49:34 -0400

    Initial Test

---
+++ pp.py
@@ -0,0 +1,14 @@
+# Create a simple streamlit app to display the data
+import streamlit as st
+import pandas as pd
+
+# Load the data
+df = pd.read_csv('./105.csv')
+
+st.tile("Public Policy Project")
+
+# Create a title
+st.title('CBO 105th Congressional')
+
+# Display the data as a table
+st.dataframe(df)
