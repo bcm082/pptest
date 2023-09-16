51ee788038bd2f66c4535c406623fa73b66599f7
bruno <bruno@pop-os.localdomain>
2023-09-15 22:03:21 -0400

    added features

--- pp.py
+++ pp.py
@@ -1,12 +1,30 @@
-# Create a simple streamlit app to display the data
 import streamlit as st
 import pandas as pd
 
-# Load the data
-df = pd.read_csv('./105.csv')
+st.title('Public Policy Data Portal')
 
-# Create a title
-st.title('CBO 105th Congressional')
+st.sidebar.header('List of Congresses')
 
-# Display the data as a table
-st.dataframe(df)
+congresses = list(range(105, 119))  # This will create a list [105, 106, ..., 118]
+
+selected_congress = st.sidebar.selectbox('Select a Congress', congresses)
+
+file_path = f'./{selected_congress}.csv'
+try:
+    df = pd.read_csv(file_path)
+
+    columns_to_search = st.multiselect("Choose columns to search:", df.columns.tolist(), default=df.columns.tolist())
+
+    search_query = st.text_input("Enter your search query:")
+
+    if search_query:
+        mask = df[columns_to_search].apply(lambda x: x.str.contains(search_query, case=False, na=False)).any(axis=1)
+        df = df[mask]
+    
+    st.dataframe(df)
+
+    st.markdown(f"Total number of bills: {len(df)}")
+
+except FileNotFoundError:
+    st.error(f"Data for Congress {selected_congress} not found.")
+
